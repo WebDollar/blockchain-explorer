663962eb70536743bd15aada1c60fccdd97a2748
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2021-09-22 11:25:57 +0300

    new option

--- src/db/db.js
+++ src/db/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const consts = require('../../consts')
 
 mongoose.connect(`mongodb+srv://${consts.db.user}:${consts.db.password}@${consts.db.server}/${consts.db.name}?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true});
+//mongoose.connect("mongodb://localhost:27018/"+consts.db.name, {useNewUrlParser: true, mohd: true} ); //for localhost
 
 const connection = mongoose.connection;
 
