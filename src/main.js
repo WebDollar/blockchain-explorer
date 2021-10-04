49248d7be6e9483d28e8f596e410906de426b1aa
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2021-10-04 23:30:59 +0300

    SSL support

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 console.log("WebDollar explorer server")
 
+const https = require('https')
+const http = require('http')
+
 const consts = require('../consts')
 const sync = require('./sync')
 const db = require('./db/db')
 const api = require('./api')
+const fs = require('fs')
 
-var express = require('express');
-var app = express();
-var cors = require('cors')
+const express = require('express');
+const app = express();
+const cors = require('cors')
 
 app.use(cors())
 
@@ -15,7 +19,9 @@ app.get('/', function (req, res) {
     res.send('Hello World');
 })
 
-var server = app.listen( consts.port, async function () {
+var server
+
+async function callback () {
 
     var host = server.address().address
     var port = server.address().port
@@ -23,4 +29,19 @@ var server = app.listen( consts.port, async function () {
 
     sync.start()
     api.initialize(app)
-})
+
+}
+
+if (fs.existsSync('certificate.pem')  ){
+    const privateKey = fs.readFileSync( 'privatekey.pem' );
+    const certificate = fs.readFileSync( 'certificate.pem' );
+
+    server = https.createServer({
+        key: privateKey,
+        cert: certificate
+    }, app).listen(consts.port, callback );
+
+}else {
+    server = http.createServer(app).listen(consts.port, callback );
+}
+
