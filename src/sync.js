16c2ef9d5415dde6cdabf223ec0874dddb3d22d0
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2021-10-08 13:49:36 +0300

    update sync #3

--- src/sync.js
+++ src/sync.js
@@ -57,7 +57,7 @@ class Sync {
         }
 
         const keys = Object.keys(allAddresses)
-        const output = await addressModel.findMany({ address: {$in: keys }} )
+        const output = await addressModel.find({ address: {$in: keys }} )
 
         const insertMissingAddresses = []
         for (let i=0; i < keys.length; i++) {
